22413a265fe8ec3f479e1eaf6eea2518763a0107
Hassan-El-Sobky <hassan_elsobky@hotmail.com>
2023-12-01 17:48:47 +0200

    abc

--- main.js
+++ main.js
@@ -61,6 +61,8 @@ class CourseUI {
 
     static showMessage(message,className)
     {
+      
+      
         const div=document.createElement('div');
         div.className=`alert alert-${className}`
         div.appendChild(document.createTextNode(message))
